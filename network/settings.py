53684de11433629f59f41dfdd281414415e8e8f2
Martin Manchev <mmanchev23@gmail.com>
2025-02-03 21:23:03 +0200

    [Fix] Static files collection

--- network/settings.py
+++ network/settings.py
@@ -110,6 +110,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
